8cf8d324ecd2e6de0cd7e3842f5d2a5602d90b2d
Devin Cooley <dcooley1350@gmail.com>
2019-10-04 14:42:58 -0700

    Write script to gather info from user and create new Counter object

---
+++ WordCounter/program.cs
@@ -0,0 +1,18 @@
+using System;
+using WordCounter.Models;
+
+namespace WordCounter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("To check a sentence or paragraph to see if it contains a keyword, first enter a keyword:");
+            string keyWord = Console.ReadLine();
+            System.Console.WriteLine("Next, enter the sentence or paragraph that you would like to check:");
+            string keySentence = Console.ReadLine();
+            Counter newCounter = new Counter(keyWord,keySentence);
+            
+        }
+    }
+}
